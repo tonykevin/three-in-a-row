4dc7e2a1c83370b8ae43b3138ba9570eb4454314
tonykevin <hi@tonykevin.com>
2022-05-13 19:00:47 -0500

    add event for mark a symbol

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 import './index.css'
 
-console.log('Bienvenido')
+function Mark(tag = {}, coordinate = { x: 0, y: 0 }, symbol = '') {
+  this.tag = tag
+  this.coordinate = coordinate
+  this.symbol = symbol
+
+  this.addSymbol = function () {
+    this.symbol = 'X'
+    this.updateTag()
+  }
+
+  this.updateTag = function () {
+    if (!this.tag.innerText) {
+      this.tag.innerText = this.symbol
+    }
+  }
+
+  this.tag.addEventListener('click', this.addSymbol.bind(this))
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  const tags = document.querySelectorAll('#mark')
+  tags.forEach(function (tag) {
+    new Mark(tag).updateTag()
+  })
+})
