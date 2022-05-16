99bfba5b12c9177475642334d5557c693ba028fc
tonykevin <hi@tonykevin.com>
2022-05-15 23:53:46 -0500

    create mark list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import { Mark } from './components'
 
 function App() {
-  const tags = document.querySelectorAll('#mark')
-  tags.forEach(function (tag) {
-    new Mark(tag).updateTag()
+  const elements = document.querySelectorAll('#mark')
+  const markList = []
+
+  elements.forEach(function (element) {
+    let mark = new Mark(element)
+    markList.push(mark)
   })
+
+  let aux = 0
+  for (let i = 1; i <= 3; i++) {
+    for (let j = 1; j <= 3; j++) {
+      markList[aux].coordinate.x = j
+      markList[aux].coordinate.y = i
+      aux++
+    }
+  }
+
+  console.log(markList)
 }
 
 export default App
