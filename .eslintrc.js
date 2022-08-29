ff1917b77f28d78c097912580aaedfa3338c1628
tonykevin <hi@tonykevin.com>
2022-08-28 23:09:21 -0500

    add a test tool: Jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,11 +2,12 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    jest: true,
   },
-  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
   rules: {},
 }
---
+++ src/__test__/global.test.js
@@ -0,0 +1,5 @@
+const numberA = 5
+
+test('should be 5', () => {
+  expect(numberA).toBe(5)
+})
